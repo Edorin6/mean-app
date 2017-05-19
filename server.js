92873ed2f1e7368e5ee9eeed4c4965a7964f20aa
Edryn Magcalas <edryn.magcalas@gmail.com>
2017-05-19 19:22:25 +0800

    deleted unwanted files

--- server.js
+++
@@ -1,8 +0,0 @@
-/**
- * @Author: Edryn Magcalas <_Edorin>
- * @Date:   17-May-2017
- * @Email:  edryn.magcalas@gmail.com
- * @Filename: server.js
- * @Last modified by:   _Edorin
- * @Last modified time: 17-May-2017
- */
